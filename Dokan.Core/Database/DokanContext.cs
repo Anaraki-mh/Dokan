1cc52af32d988b1818c9fd610714e54592b24b0c
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-19 03:51:01 +0330

    Relocated and split IdentityModel to Core and Domain layers

---
+++ Dokan.Core/Database/DokanContext.cs
@@ -0,0 +1,23 @@
+﻿using Dokan.Domain.UsersAndRoles;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Core.Database
+{
+    public class DokanContext : IdentityDbContext<User>
+    {
+        public DokanContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static DokanContext Create()
+        {
+            return new DokanContext();
+        }
+    }
+}
---
+++ Dokan.Domain/UsersAndRoles/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.UsersAndRoles
+{
+    public class User : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
