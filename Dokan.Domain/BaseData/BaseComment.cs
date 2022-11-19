a53a04298f347840f08dea20a876acf141fc173f
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-19 05:03:43 +0330

    Added BaseComment for comments to inherit the base properties from

---
+++ Dokan.Domain/BaseData/BaseComment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.BaseData
+{
+    public class BaseComment : BaseEntity
+    {
+        [MaxLength(40)]
+        public string Title { get; set; }
+
+        [MaxLength(300)]
+        public string Body { get; set; }
+
+        public bool IsApproved { get; set; }
+    }
+}
