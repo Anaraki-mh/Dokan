6d2246178c10f46051a8ebaf9fff69f14dd19384
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-19 04:57:01 +0330

    Added BaseEntity for entities to inherit the base properties from

---
+++ Dokan.Domain/BaseData/BaseEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.BaseData
+{
+    public class BaseEntity
+    {
+
+        #region Constructor
+
+        public BaseEntity()
+        {
+            CreateDateTime = DateTime.UtcNow;
+            UpdateDateTime = DateTime.UtcNow;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [Key]
+        public int Id { get; set; }
+        public bool IsRemoved { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+
+        #endregion
+
+    }
+}
