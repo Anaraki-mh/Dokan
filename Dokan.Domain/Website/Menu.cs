a5ef8d0c2d6d06b9b86a96813d28b39095181180
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-24 01:08:22 +0330

    Added Menu entity

---
+++ Dokan.Domain/Website/Menu.cs
@@ -0,0 +1,32 @@
+﻿using Dokan.Domain.BaseData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class Menu : BaseEntity
+    {
+
+        #region Properties
+
+        [MaxLength(45)]
+        public string Title { get; set; }
+
+        [MaxLength(75)]
+        public string Link { get; set; }
+
+        #endregion
+
+
+        #region Relations
+
+        public int? ParentId { get; set; }
+        public virtual Menu Parent { get; set; }
+
+        #endregion
+    }
+}
