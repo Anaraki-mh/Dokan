3cc13732409fa2969c2a1fba5e204a5ff1ce28da
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-24 02:03:24 +0330

    Added KeyValueContentType to indicate the type of the key value pairs (which are going to be added next)

---
+++ Dokan.Domain/Enums/KeyValueContentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Enums
+{
+    public enum KeyValueContentType
+    {
+        HTML,
+        Text,
+        Image,
+        Video,
+        Audio,
+    }
+}
