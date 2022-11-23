275313046ffb5623ca8af6344bc0bc997db45115
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-24 02:07:51 +0330

    Added KeyValueContent entity

---
+++ Dokan.Domain/Website/KeyValueContent.cs
@@ -0,0 +1,27 @@
+﻿using Dokan.Domain.BaseData;
+using Dokan.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Dokan.Domain.Website
+{
+    public class KeyValueContent : BaseEntity
+    {
+        [MaxLength(50)]
+        public string ContentKey { get; set; }
+
+        [AllowHtml]
+        [MaxLength(1000)]
+        public string ContentValue { get; set; }
+
+        [MaxLength(75)]
+        public string Description { get; set; }
+
+        public KeyValueContentType ContentType { get; set; }
+    }
+}
