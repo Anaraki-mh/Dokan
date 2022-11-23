ea2a57d985d188506c486df9c77146ea41f1e492
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-24 00:08:44 +0330

    Added Order and OrderItem entities

---
+++ Dokan.Domain/Website/Order.cs
@@ -0,0 +1,51 @@
+﻿using Dokan.Domain.Enums;
+using Dokan.Domain.UsersAndRoles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class Order
+    {
+        #region Constructor
+
+        public Order()
+        {
+            OrderState = OrderState.Incomplete;
+            OrderItems = new List<OrderItem>();
+            CreateDateTime = DateTime.UtcNow;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+
+        [MaxLength(50)]
+        public string TrackingCode { get; set; }
+
+        public OrderState OrderState { get; set; }
+
+        #endregion
+
+
+        #region Relations
+
+        [MaxLength(100)]
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual List<OrderItem> OrderItems { get; set; }
+
+        #endregion
+    }
+}
---
+++ Dokan.Domain/Website/OrderItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class OrderItem
+    {
+        #region Constructor
+
+        public OrderItem()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [Key]
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public int Discount { get; set; }
+        public int Quantity { get; set; }
+        public double Total { get; set; }
+
+        #endregion
+
+
+        #region Relations
+
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public int OrderId { get; set; }
+        public virtual Order Order { get; set; }
+
+        #endregion
+    }
+}
