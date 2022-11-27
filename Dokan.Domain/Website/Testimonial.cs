47f7996f5178fe942cf32af5e8b08a72b8d6d986
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-27 21:38:14 +0330

    Added Testimonial entity

---
+++ Dokan.Domain/Website/Testimonial.cs
@@ -0,0 +1,34 @@
+﻿using Dokan.Domain.BaseData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class Testimonial : BaseEntity
+    {
+        #region Properties
+
+        [MaxLength(50)]
+        public string FullName { get; set; }
+
+        [MaxLength(35)]
+        public string Position { get; set; }
+
+        [MaxLength(300)]
+        public string Content { get; set; }
+
+        #endregion
+
+
+        #region 
+
+        public int ImageId { get; set; }
+        public virtual File Image { get; set; }
+
+        #endregion
+    }
+}
