510f270351b277c1e64def39fa7d1f6782af80ff
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-29 15:14:26 +0330

    Added Log entity and LogType enum

---
+++ Dokan.Domain/Enums/LogType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Enums
+{
+    public enum LogType
+    {
+        Error = -1,
+        Process = 0,
+        ContentAdd = 10,
+        ContentUpdate = 11,
+        ContentDelete = 12,
+    }
+}
---
+++ Dokan.Domain/Website/Log.cs
@@ -0,0 +1,47 @@
+﻿using Dokan.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class Log
+    {
+        #region Constructor
+
+        public Log()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [Key]
+        public int Id { get; set; }
+
+        public LogType LogType { get; set; }
+
+        [MaxLength(100)]
+        public string Controller { get; set; }
+
+        [MaxLength(100)]
+        public string Method { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [MaxLength(50)]
+        public string Code { get; set; }
+
+        [MaxLength(500)]
+        public string AdditionalInfo { get; set; }
+
+        #endregion
+    }
+}
