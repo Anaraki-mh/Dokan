223e5292ca4c3d7efe730ed398985610040b1c81
AnarakiMh <m.anaraki.1382@gmail.com>
2022-11-29 18:39:31 +0330

    Fixed a bug related to ProductPricingRule entity (not inheriting from BaseEntity)

--- Dokan.Domain/Website/ProductPricingRule.cs
+++ Dokan.Domain/Website/ProductPricingRule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dokan.Domain.BaseData;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Dokan.Domain.Website
 {
-    public class ProductPricingRule
+    public class ProductPricingRule : BaseEntity
     {
         #region Constructor
 
