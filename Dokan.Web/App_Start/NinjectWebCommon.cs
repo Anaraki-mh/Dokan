02310d0876aadb000a516ee014bce1c222c114ce
AnarakiMh <m.anaraki.1382@gmail.com>
2022-12-06 23:36:01 +0330

    Added and set up Ninject for dependency injection

---
+++ Dokan.Web/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,98 @@
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Dokan.Web.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Dokan.Web.App_Start.NinjectWebCommon), "Stop")]
+
+namespace Dokan.Web.App_Start
+{
+    using System;
+    using System.Data.Entity;
+    using System.Web;
+    using Dokan.Core.DataAccess;
+    using Dokan.Core.Database;
+    using Dokan.Services;
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<DbContext>().To<DokanContext>().InRequestScope();
+            kernel.Bind(typeof(IRepository<>)).To(typeof(Repository<>));
+
+            kernel.Bind<IUserService>().To<UserService>();
+            kernel.Bind<IUserInformationService>().To<UserInformationService>();
+
+            kernel.Bind<IBlogPostService>().To<BlogPostService>();
+            kernel.Bind<IBlogCategoryService>().To<BlogCategoryService>();
+            kernel.Bind<IBlogCommentService>().To<BlogCommentService>();
+
+            kernel.Bind<IProductService>().To<ProductService>();
+            kernel.Bind<IProductCategoryService>().To<ProductCategoryService>();
+            kernel.Bind<IProductCommentService>().To<ProductCommentService>();
+
+            kernel.Bind<IProductPricingRuleService>().To<ProductPricingRuleService>();
+            kernel.Bind<IProductCategoryPricingRuleService>().To<ProductCategoryPricingRuleService>();
+
+            kernel.Bind<ICouponService>().To<CouponService>();
+
+            kernel.Bind<IOrderService>().To<OrderService>();
+            kernel.Bind<IOrderItemService>().To<OrderItemService>();
+
+            kernel.Bind<IFileService>().To<FileService>();
+            kernel.Bind<ILogService>().To<LogService>();
+
+            kernel.Bind<IMenuService>().To<MenuService>();
+            kernel.Bind<ICarouselService>().To<CarouselService>();
+            kernel.Bind<ITestimonialService>().To<TestimonialService>();
+            kernel.Bind<IKeyValueContentService>().To<KeyValueContentService>();
+        }
+    }
+}
