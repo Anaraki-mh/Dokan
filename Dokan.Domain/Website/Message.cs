051864c87aed2b2ee16a44caad6860096bd0b86e
AnarakiMh <m.anaraki.1382@gmail.com>
2022-12-11 22:19:25 +0330

    Added Message entity to save contact us messages

---
+++ Dokan.Domain/Website/Message.cs
@@ -0,0 +1,39 @@
+﻿using Dokan.Domain.BaseData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class Message : BaseEntity
+    {
+        #region Constructor
+
+        public Message()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [MaxLength(75)]
+        public string Name { get; set; }
+
+        [MaxLength(75)]
+        public string Email { get; set; }
+
+        [MaxLength(75)]
+        public string Subject { get; set; }
+
+        [MaxLength(400)]
+        public string MessageBody { get; set; }
+
+        #endregion
+    }
+}
