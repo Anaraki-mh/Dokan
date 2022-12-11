34b9e364cdd1f87ac694c232bb83c00207cbcb10
AnarakiMh <m.anaraki.1382@gmail.com>
2022-12-11 21:00:51 +0330

    Changed ProductCategoryPricingRule and ProductPricingRule entities to TaxCategory and DiscountCategory

---
+++ Dokan.Domain/Website/DiscountCategory.cs
@@ -0,0 +1,40 @@
+﻿using Dokan.Domain.BaseData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class DiscountCategory : BaseEntity
+    {
+        #region Constructor
+
+        public DiscountCategory()
+        {
+            Products = new List<Product>();
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [MaxLength(75)]
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ExpiryDateTime { get; set; }
+        public int Discount { get; set; }
+
+        #endregion
+
+
+        #region Relations
+
+        public virtual List<Product> Products { get; set; }
+
+        #endregion
+    }
+}
---
+++ Dokan.Domain/Website/TaxCategory.cs
@@ -0,0 +1,39 @@
+﻿using Dokan.Domain.BaseData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.Domain.Website
+{
+    public class TaxCategory : BaseEntity
+    {
+        #region Constructor
+
+        public TaxCategory()
+        {
+            ProductCategories = new List<ProductCategory>();
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        [MaxLength(75)]
+        public string Title { get; set; }
+
+        public int Tax { get; set; }
+
+        #endregion
+
+
+        #region Relations
+
+        public virtual List<ProductCategory> ProductCategories { get; set; }
+
+        #endregion
+    }
+}
