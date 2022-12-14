0b86e171a7b40e2b2eb8113f665d276356a3fce1
Anaraki-mh <m.anaraki.1382@gmail.com>
2022-12-14 21:25:23 +0330

    Added BlogCategoryModel to transfer data between the front-end and the database and services

---
+++ Dokan.Web/Models/BlogCategoryModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages.Html;
+using SelectListItem = System.Web.WebPages.Html.SelectListItem;
+
+namespace Dokan.Web.Models
+{
+    public class BlogCategoryModel
+    {
+        #region Constructor
+
+        public BlogCategoryModel()
+        {
+            ParentDropdown = new List<SelectListItem>(new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text= "Select an item...",
+                    Value = "",
+                    Selected = true
+                }
+            });
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        [Display(Name = "Create/Update date")]
+        public DateTime UpdateDateTime { get; set; }
+
+        public int index { get; set; }
+
+        [Required(ErrorMessage = "{0} can not be empty")]
+        [MaxLength(40, ErrorMessage = "{0} can not be longer than {1} characters")]
+        public string Title { get; set; }
+
+        [Display(Name = "Display priority")]
+        public int? Priority { get; set; }
+
+        [Display(Name = "Parent Category")]
+        public int? ParentId { get; set; }
+
+        public string ParentTitle { get; set; }
+
+        public List<SelectListItem> ParentDropdown { get; set; }
+
+        #endregion
+    }
+}
