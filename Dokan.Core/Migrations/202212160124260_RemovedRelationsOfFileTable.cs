45c1daf0b60d0f9bed3b5b10da3bdc2ecc8300a9
Anaraki-mh <m.anaraki.1382@gmail.com>
2022-12-16 04:57:11 +0330

    Added new migration to apply the changes to the database

---
+++ Dokan.Core/Migrations/202212160124260_RemovedRelationsOfFileTable.cs
@@ -0,0 +1,60 @@
+﻿namespace Dokan.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovedRelationsOfFileTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.BlogPosts", "ImageId", "dbo.Files");
+            DropForeignKey("dbo.FileProducts", "File_Id", "dbo.Files");
+            DropForeignKey("dbo.FileProducts", "Product_Id", "dbo.Products");
+            DropForeignKey("dbo.Testimonials", "ImageId", "dbo.Files");
+            DropIndex("dbo.BlogPosts", new[] { "ImageId" });
+            DropIndex("dbo.Testimonials", new[] { "ImageId" });
+            DropIndex("dbo.FileProducts", new[] { "File_Id" });
+            DropIndex("dbo.FileProducts", new[] { "Product_Id" });
+            AddColumn("dbo.BlogPosts", "Image", c => c.String(maxLength: 30));
+            AddColumn("dbo.Products", "Image1", c => c.String(maxLength: 30));
+            AddColumn("dbo.Products", "Image2", c => c.String(maxLength: 30));
+            AddColumn("dbo.Products", "Image3", c => c.String(maxLength: 30));
+            AddColumn("dbo.Products", "Image4", c => c.String(maxLength: 30));
+            AddColumn("dbo.Products", "Image5", c => c.String(maxLength: 30));
+            AddColumn("dbo.Testimonials", "Image", c => c.String(maxLength: 30));
+            DropColumn("dbo.BlogPosts", "ImageId");
+            DropColumn("dbo.Testimonials", "ImageId");
+            DropTable("dbo.FileProducts");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.FileProducts",
+                c => new
+                    {
+                        File_Id = c.Int(nullable: false),
+                        Product_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.File_Id, t.Product_Id });
+            
+            AddColumn("dbo.Testimonials", "ImageId", c => c.Int(nullable: false));
+            AddColumn("dbo.BlogPosts", "ImageId", c => c.Int(nullable: false));
+            DropColumn("dbo.Testimonials", "Image");
+            DropColumn("dbo.Products", "Image5");
+            DropColumn("dbo.Products", "Image4");
+            DropColumn("dbo.Products", "Image3");
+            DropColumn("dbo.Products", "Image2");
+            DropColumn("dbo.Products", "Image1");
+            DropColumn("dbo.BlogPosts", "Image");
+            CreateIndex("dbo.FileProducts", "Product_Id");
+            CreateIndex("dbo.FileProducts", "File_Id");
+            CreateIndex("dbo.Testimonials", "ImageId");
+            CreateIndex("dbo.BlogPosts", "ImageId");
+            AddForeignKey("dbo.Testimonials", "ImageId", "dbo.Files", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.FileProducts", "Product_Id", "dbo.Products", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.FileProducts", "File_Id", "dbo.Files", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.BlogPosts", "ImageId", "dbo.Files", "Id", cascadeDelete: true);
+        }
+    }
+}
