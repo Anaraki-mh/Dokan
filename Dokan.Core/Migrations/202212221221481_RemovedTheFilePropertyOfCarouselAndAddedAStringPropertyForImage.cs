9fe20ae9441b18b4958016f412ea206d1cc6659b
Anaraki-mh <m.anaraki.1382@gmail.com>
2022-12-22 15:53:14 +0330

    Added a new migration to apply the changes made to Carousel Table

---
+++ Dokan.Core/Migrations/202212221221481_RemovedTheFilePropertyOfCarouselAndAddedAStringPropertyForImage.cs
@@ -0,0 +1,28 @@
+﻿namespace Dokan.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovedTheFilePropertyOfCarouselAndAddedAStringPropertyForImage : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Carousels", "ImageId", "dbo.Files");
+            DropIndex("dbo.Carousels", new[] { "ImageId" });
+            AddColumn("dbo.Carousels", "IsDisplayed", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Carousels", "Image", c => c.String(maxLength: 30));
+            DropColumn("dbo.Carousels", "Display");
+            DropColumn("dbo.Carousels", "ImageId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Carousels", "ImageId", c => c.Int(nullable: false));
+            AddColumn("dbo.Carousels", "Display", c => c.Boolean(nullable: false));
+            DropColumn("dbo.Carousels", "Image");
+            DropColumn("dbo.Carousels", "IsDisplayed");
+            CreateIndex("dbo.Carousels", "ImageId");
+            AddForeignKey("dbo.Carousels", "ImageId", "dbo.Files", "Id", cascadeDelete: true);
+        }
+    }
+}
