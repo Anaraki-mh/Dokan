b6ae9e397e89bac0a93bfaa159ad0eb4f597ebf9
Anaraki-mh <m.anaraki.1382@gmail.com>
2022-12-22 14:36:24 +0330

    Added new migration to apply the changes made to the Menu Table

---
+++ Dokan.Core/Migrations/202212220956430_AddedPriorityPropertyToMenu.cs
@@ -0,0 +1,18 @@
+﻿namespace Dokan.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedPriorityPropertyToMenu : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Menus", "Priority", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Menus", "Priority");
+        }
+    }
+}
