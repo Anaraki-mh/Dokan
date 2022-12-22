26ad1d128cc5da68553cd1c441faae5398b20c0c
Anaraki-mh <m.anaraki.1382@gmail.com>
2022-12-22 14:54:36 +0330

    Added MenuModel and controller

---
+++ Dokan.Web/Areas/Management/Controllers/MenuController.cs
@@ -0,0 +1,388 @@
+﻿using Dokan.Domain.Enums;
+using Dokan.Domain.Website;
+using Dokan.Services;
+using Dokan.Web.Areas.Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace Dokan.Web.Areas.Management.Controllers
+{
+    public class MenuController : Controller
+    {
+        #region Properties and fields
+
+        private IMenuService _menuService { get; }
+        private ILogService _logService { get; }
+
+        private List<Menu> _allEntities { get; set; }
+        private MenuModel _model;
+        private Menu _entity;
+
+        #endregion
+
+
+        #region Constructor
+
+        public MenuController(IMenuService MenuService, ILogService logService)
+        {
+            _menuService = MenuService;
+            _logService = logService;
+
+            _allEntities = new List<Menu>();
+            _model = new MenuModel();
+            _entity = new Menu();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> List(int page = 1, int numberOfResults = 5)
+        {
+            List<MenuModel> convertedEntityList = new List<MenuModel>();
+
+            _allEntities = await _menuService.ListAsync();
+
+            List<Menu> filteredList = _allEntities.Skip((page - 1) * numberOfResults).Take(numberOfResults).ToList();
+
+            int index = (page - 1) * numberOfResults + 1;
+
+            foreach (var entity in filteredList)
+            {
+                _model = new MenuModel();
+                EntityToModel(entity, ref _model, index);
+
+                convertedEntityList.Add(_model);
+
+                index++;
+            }
+
+            ViewBag.NumberOfPages = Math.Ceiling((decimal)_allEntities.Count / (decimal)numberOfResults);
+            ViewBag.ActivePage = page;
+
+            return PartialView("_List", convertedEntityList);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(int id)
+        {
+            _entity = await _menuService.FindByIdAsync(id);
+
+            EntityToModel(_entity, ref _model);
+
+            return PartialView("_Details", _model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            EmptyModel(ref _model);
+            PrepareDropdown(ref _model, await _menuService.ListAsync());
+
+            return View(_model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(MenuModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            try
+            {
+                ModelToEntity(model, ref _entity);
+                await _menuService.CreateAsync(_entity);
+
+                await Log(LogType.ContentAdd, "Create", $"{_entity.Id}_ {_entity.Title}");
+            }
+            catch (Exception ex)
+            {
+                await LogError(ex);
+
+                return View("Error400");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Update(int id)
+        {
+            try
+            {
+                _entity = await _menuService.FindByIdAsync(id);
+            }
+            catch (Exception ex)
+            {
+                await LogError(ex);
+
+                return View("Error400");
+            }
+
+            EntityToModel(_entity, ref _model);
+
+            PrepareDropdown(ref _model, await _menuService.ListAsync());
+
+            return View(_model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Update(MenuModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                PrepareDropdown(ref model, await _menuService.ListAsync());
+                return View(model);
+            }
+
+            try
+            {
+                ModelToEntity(model, ref _entity);
+                await _menuService.UpdateAsync(_entity);
+
+                await Log(LogType.ContentUpdate, "Update", $"{_entity.Id}_ {_entity.Title}");
+            }
+            catch (Exception ex)
+            {
+                await LogError(ex);
+
+                return View("Error400");
+            }
+
+            return RedirectToAction("Index", "Menu");
+        }
+
+        [HttpGet]
+        public ActionResult Trash()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> TrashList()
+        {
+            List<MenuModel> convertedEntityList = new List<MenuModel>();
+            List<Menu> removedEntityList = await _menuService.ListOfRemovedAsync();
+
+            int index = 1;
+
+            foreach (var entity in removedEntityList)
+            {
+                _model = new MenuModel();
+                EntityToModel(entity, ref _model, index);
+
+                convertedEntityList.Add(_model);
+
+                index++;
+            }
+
+
+            return PartialView("_TrashList" ,convertedEntityList);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                await _menuService.DeleteAsync(id);
+
+                await Log(LogType.ContentDelete, "Delete", $"{_entity.Id}_ {_entity.Title}");
+            }
+            catch (Exception ex)
+            {
+                await LogError(ex);
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Remove(int id)
+        {
+            try
+            {
+                await _menuService.RemoveAsync(id);
+            }
+            catch (Exception ex)
+            {
+                await LogError(ex);
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Restore(int id)
+        {
+            try
+            {
+                await _menuService.RestoreAsync(id);
+            }
+            catch (Exception ex)
+            {
+                await LogError(ex);
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DeleteAllTrash()
+        {
+            try
+            {
+                await _menuService.DeleteRangeAsync(await _menuService.ListOfRemovedAsync());
+                await Log(LogType.ContentAdd, "DeleteAllTrash", $"Deleted all items in the trash");
+            }
+            catch (Exception ex)
+            {
+                await LogError(ex);
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        #endregion
+
+
+        #region Conversion Methods
+
+        private void EntityToModel(Menu entity, ref MenuModel model)
+        {
+            EmptyModel(ref model);
+
+            model.Id = entity.Id;
+            model.Title = entity.Title;
+            model.Link = entity.Link;
+            model.Priority = entity.Priority;
+            model.ParentId = entity.ParentId;
+            model.ParentTitle = entity.Parent?.Title ?? " - ";
+            model.UpdateDateTime = entity.UpdateDateTime;
+        }
+
+        private void EntityToModel(Menu entity, ref MenuModel model, int index)
+        {
+            EmptyModel(ref model);
+
+            model.Id = entity.Id;
+            model.index = index;
+            model.Title = entity.Title;
+            model.Link = entity.Link;
+            model.Priority = entity.Priority;
+            model.ParentId = entity.ParentId;
+            model.ParentTitle = entity.Parent?.Title ?? " - ";
+            model.UpdateDateTime = entity.UpdateDateTime;
+        }
+
+        private void ModelToEntity(MenuModel model, ref Menu entity)
+        {
+            EmptyEntity(ref entity);
+
+            entity.Id = model.Id;
+            entity.Title = model.Title;
+            entity.Link = model.Link;
+            entity.Priority = model.Priority ?? 0;
+            entity.ParentId = model.ParentId;
+        }
+
+        private void EmptyEntity(ref Menu entity)
+        {
+            entity.Id = 0;
+            entity.Title = "";
+            entity.Link = "";
+            entity.Priority = 0;
+            entity.ParentId = 0;
+        }
+
+        private void EmptyModel(ref MenuModel model)
+        {
+            model.Id = 0;
+            model.index = 0;
+            model.Title = "";
+            model.Link = "";
+            model.Priority = 0;
+            model.ParentId = 0;
+            model.ParentTitle = "";
+            model.ParentDropdown.Clear();
+        }
+
+        #endregion
+
+
+        #region Log and Preperation Methods
+
+        private async Task LogError(Exception ex)
+        {
+            await _logService.CreateAsync(new Log()
+            {
+                LogType = LogType.Error,
+                Controller = ex.Source,
+                Method = ex.TargetSite.Name,
+                Description = ex.Message,
+                AdditionalInfo = ex.InnerException.ToString(),
+                Code = ex.HResult.ToString(),
+            });
+        }
+
+        private async Task Log(LogType logType, string method, string description)
+        {
+            await _logService.CreateAsync(new Log()
+            {
+                LogType = logType,
+                Controller = this.GetType().Name,
+                Method = method,
+                Description = $"{logType} _ {description}",
+            });
+        }
+
+        private void PrepareDropdown(ref MenuModel model, List<Menu> dropdownItemsList)
+        {
+            model.ParentDropdown.Clear();
+
+            model.ParentDropdown.Add(new SelectListItem()
+            {
+                Text = "Select an item...",
+                Value = "",
+            });
+
+            int  modelId = model.Id;
+            dropdownItemsList.Remove(dropdownItemsList.FirstOrDefault(x => x.Id == modelId));
+
+            foreach (var entity in dropdownItemsList)
+            {
+                model.ParentDropdown.Add(new SelectListItem()
+                {
+                    Text = entity.Title,
+                    Value = entity.Id.ToString(),
+                });
+            }
+        }
+        
+        #endregion
+    }
+}
---
+++ Dokan.Web/Areas/Management/Models/MenuModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages.Html;
+using SelectListItem = System.Web.Mvc.SelectListItem;
+
+namespace Dokan.Web.Areas.Management.Models
+{
+    public class MenuModel
+    {
+        #region Constructor
+
+        public MenuModel()
+        {
+            ParentDropdown = new List<SelectListItem>();
+        }
+
+        #endregion
+
+
+        #region Properties
+        public int index { get; set; }
+
+        public int Id { get; set; }
+
+        [Display(Name = "Create/Update date")]
+        public DateTime UpdateDateTime { get; set; }
+
+
+        [Required(ErrorMessage = "{0} can not be empty")]
+        [MaxLength(40, ErrorMessage = "{0} can not be longer than {1} characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "{0} can not be empty")]
+        [MaxLength(75, ErrorMessage = "{0} can not be longer than {1} characters")]
+        public string Link { get; set; }
+
+        [Display(Name = "Display priority")]
+        public int? Priority { get; set; }
+
+        [Display(Name = "Parent")]
+        public int? ParentId { get; set; }
+
+        public string ParentTitle { get; set; }
+
+        public List<SelectListItem> ParentDropdown { get; set; }
+
+        #endregion
+    }
+}
