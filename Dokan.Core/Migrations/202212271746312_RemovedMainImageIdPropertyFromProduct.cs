e6e55887762511e2236344f37ae8592004c52559
Anaraki-mh <m.anaraki.1382@gmail.com>
2022-12-27 21:18:34 +0330

    Added new migration to apply the changes made to Product table

---
+++ Dokan.Core/Migrations/202212271746312_RemovedMainImageIdPropertyFromProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace Dokan.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovedMainImageIdPropertyFromProduct : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Products", "MainImageId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "MainImageId", c => c.Int(nullable: false));
+        }
+    }
+}
