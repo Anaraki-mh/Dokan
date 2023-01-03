5b5313f6d0188a4ff66ccc72462bd71a34c72889
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-01-04 02:17:32 +0330

    Added a Typography helper with a TrimString method

---
+++ Dokan.WebEssentials/Typography.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dokan.WebEssentials
+{
+    public static class Typography
+    {
+        public static string TrimString(string stringData, int length)
+        {
+            if(stringData.Length > length - 3)
+            {
+                stringData = stringData.Substring(length - 3) + "...";
+            }
+            return stringData;
+        }
+    }
+}
