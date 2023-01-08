fc3fe9e9d8cbada9705eb2183e888ede97be02e1
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-01-08 04:10:19 +0330

    Added an EmailService and its interface to send emails using the smtp client

---
+++ Dokan.Services/EmailService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace Dokan.Services
+{
+    public class EmailService : IEmailService
+    {
+        #region Fields and Properties
+
+        private SmtpClient _smtpClient { get; }
+        private string _hostName { get; }
+        private string _username { get; }
+        private string _password { get; }
+        private string _senderEmail { get; }
+
+        #endregion
+
+
+        #region Constructor
+
+        public EmailService()
+        {
+            _hostName = "smtp.gmail.com";
+            _username = WebConfigurationManager.AppSettings["EmailUsername"];
+            _password = WebConfigurationManager.AppSettings["EmailPassword"];
+            _senderEmail = "";
+
+            _smtpClient = new SmtpClient(_hostName)
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(_username, _password),
+                EnableSsl = true,
+            };
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void SendEmail(string subject, string body, string recipientEmail)
+        {
+            try
+            {
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress(_senderEmail),
+                    Subject = subject,
+                    Body = body,
+                    IsBodyHtml = true,
+
+                };
+
+                mailMessage.To.Add(recipientEmail);
+
+                _smtpClient.Send(mailMessage);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Dokan.Services/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace Dokan.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(string subject, string body, string recipientEmail);
+    }
+}
