6f4d1fcc5326fad0e6bac65479c58eefe16f866a
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-01-10 00:55:48 +0330

    Added a new helper named SEO with a method to convert product and blogpost titles to url-friendly strings

---
+++ Dokan.WebEssentials/SEO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace Dokan.WebEssentials
+{
+    public static class SEO
+    {
+        private static List<char> UnwiseOrReservedChars = new List<char> 
+        {
+            ' ' ,'{' , '}' , '|' , '\\' , '^' , '[' , ']' , '`', '<', '>', ';' , '/' , '?' , ':' , '@' , '&' , '=' , '+' , '$' , ','
+        };
+
+        public static string CreateSeoFriendlyUrlTitle(string title)
+        {
+            foreach (var character in UnwiseOrReservedChars)
+            {
+                title = title.Replace(character, '-');
+            }
+            return title;
+        }
+    }
+}
