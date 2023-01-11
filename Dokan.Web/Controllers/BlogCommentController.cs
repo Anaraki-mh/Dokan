2f759b2f55198ac9c550bda56959eecb8f4ffb97
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-01-11 03:41:01 +0330

    Added Product and Blog Comment controllers and models for the main area

---
+++ Dokan.Web/Controllers/BlogCommentController.cs
@@ -0,0 +1,159 @@
+﻿using Dokan.Domain.Website;
+using Dokan.Services;
+using Dokan.Web.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dokan.Web.Controllers
+{
+    public class BlogCommentController : Controller
+    {
+        #region Fields and Properties
+
+        private IBlogCommentService _blogCommentService { get; }
+        private IEmailService _emailService { get; }
+
+        private BlogCommentModel _model;
+        private BlogComment _entity;
+        private List<BlogComment> _allEntities { get; set; }
+
+        #endregion
+
+
+        #region Contrsuctor
+
+        public BlogCommentController(IBlogCommentService blogCommentService, IEmailService emailService)
+        {
+            _blogCommentService = blogCommentService;
+            _emailService = emailService;
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public async Task<ActionResult> List(int page = 1)
+        {
+            int numberOfResults = 10;
+
+            List<BlogCommentModel> convertedEntityList = new List<BlogCommentModel>();
+
+            _allEntities = await _blogCommentService.ListAsync();
+
+            _allEntities = _allEntities.Skip((page - 1) * numberOfResults).Take(numberOfResults).ToList();
+
+            int index = (page - 1) * numberOfResults + 1;
+
+            foreach (var entity in _allEntities)
+            {
+                _model = new BlogCommentModel();
+                EntityToModel(entity, ref _model);
+
+                convertedEntityList.Add(_model);
+
+                index++;
+            }
+
+            ViewBag.NumberOfPages = Math.Ceiling((decimal)_allEntities.Count / (decimal)numberOfResults);
+            ViewBag.ActivePage = page;
+
+            return View("_List", convertedEntityList);
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView("_Create");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(BlogCommentModel model)
+        {
+            if (!ModelState.IsValid)
+                return PartialView("_Create");
+
+            ModelToEntity(model, ref _entity);
+
+            await _blogCommentService.CreateAsync(_entity);
+
+            return PartialView("_Created");
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            _entity = await _blogCommentService.FindByIdAsync(id);
+
+            if (HttpContext.User.Identity.GetUserId() == _entity.UserId)
+                await _blogCommentService.DeleteAsync(id);
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+        public async Task<ActionResult> Reply(int id)
+        {
+            _entity = await _blogCommentService.FindByIdAsync(id);
+
+            if (_entity.Id == id)
+            {
+                BlogCommentModel model = new BlogCommentModel();
+                model.ParentId = id;
+                model.BlogPostId = _entity.BlogPostId;
+                model.UserId = HttpContext.User.Identity.GetUserId();
+
+                return PartialView(model);
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Reply(BlogCommentModel model)
+        {
+            if (!ModelState.IsValid)
+                return PartialView("_Create");
+
+            ModelToEntity(model, ref _entity);
+
+            await _blogCommentService.CreateAsync(_entity);
+
+            return PartialView("_Created");
+        }
+
+        #endregion
+
+
+        #region Conversion Methods
+
+        private void EntityToModel(BlogComment entity, ref BlogCommentModel model)
+        {
+            model.Id = entity.Id;
+            model.Username = entity.User?.UserName;
+            model.UserId = entity.UserId;
+            model.BlogPostId = entity.BlogPostId;
+            model.Body = entity.Body;
+            model.Rating = entity.Rating;
+            model.CreateDateTime = entity.CreateDateTime;
+        }
+
+        private void ModelToEntity(BlogCommentModel model, ref BlogComment entity)
+        {
+            entity.Id = model.Id;
+            entity.UserId = model.UserId;
+            entity.BlogPostId = model.BlogPostId;
+            entity.Body = model.Body;
+            entity.Rating = model.Rating;
+            entity.CreateDateTime = DateTime.UtcNow;
+            entity.UpdateDateTime = DateTime.UtcNow;
+        }
+
+
+        #endregion
+    }
+}
---
+++ Dokan.Web/Models/ProductCommentModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages.Html;
+using SelectListItem = System.Web.Mvc.SelectListItem;
+
+namespace Dokan.Web.Models
+{
+    public class ProductCommentModel
+    {
+        #region Constructor
+
+        public ProductCommentModel()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Properties
+        public int Index { get; set; }
+
+        public int Id { get; set; }
+
+        [Display(Name = "Create date")]
+        public DateTime CreateDateTime { get; set; }
+
+        public int ProductId { get; set; }
+        public int ParentId { get; set; }
+
+        [Display(Name = "Username")]
+        [MaxLength(40, ErrorMessage = "{0} can not be longer than {1} characters")]
+        public string Username { get; set; }
+
+        public string UserId { get; set; }
+
+        [Display(Name = "Comment Body")]
+        [MaxLength(300, ErrorMessage = "{0} can not be longer than {1} characters")]
+        public string Body { get; set; }
+
+        [Display(Name = "Rating")]
+        [Range(0,5, ErrorMessage = "{0} can not be less than {1} and greater {2}")]
+        public int Rating { get; set; }
+
+        #endregion
+    }
+}
