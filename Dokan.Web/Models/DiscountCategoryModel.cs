cd6573df4735ae893f825410678b15f797b283c4
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-01-15 01:17:23 +0330

    Added a DiscountCategoryModel for the main area

---
+++ Dokan.Web/Models/DiscountCategoryModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebPages.Html;
+using SelectListItem = System.Web.Mvc.SelectListItem;
+
+namespace Dokan.Web.Models
+{
+    public class DiscountCategoryModel
+    {
+        #region Constructor
+
+        public DiscountCategoryModel()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        [Display(Name = "Time Left Until Expiration")]
+        public TimeSpan UntilExpiration { get; set; }
+
+        [Display(Name = "Title")]
+        [Required(ErrorMessage = "{0} can not be empty")]
+        [MaxLength(75, ErrorMessage = "{0} can not be longer than {1} characters")]
+        public string Title { get; set; }
+
+        [Display(Name = "Discount")]
+        [Required(ErrorMessage = "{0} can not be empty")]
+        [Range(0,100, ErrorMessage = "{0} can not be less than {1} and greater {2}")]
+        public int Discount { get; set; }
+
+        #endregion
+    }
+}
