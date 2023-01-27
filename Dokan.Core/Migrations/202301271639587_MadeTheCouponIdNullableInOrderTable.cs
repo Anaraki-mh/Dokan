8553c6a686fc7bd07f434706b577f2262ed39f23
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-01-27 20:13:06 +0330

    Changed the CouponId property of Order entity so that it can be null

---
+++ Dokan.Core/Migrations/202301271639587_MadeTheCouponIdNullableInOrderTable.cs
@@ -0,0 +1,26 @@
+﻿namespace Dokan.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MadeTheCouponIdNullableInOrderTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Orders", "CouponId", "dbo.Coupons");
+            DropIndex("dbo.Orders", new[] { "CouponId" });
+            AlterColumn("dbo.Orders", "CouponId", c => c.Int());
+            CreateIndex("dbo.Orders", "CouponId");
+            AddForeignKey("dbo.Orders", "CouponId", "dbo.Coupons", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Orders", "CouponId", "dbo.Coupons");
+            DropIndex("dbo.Orders", new[] { "CouponId" });
+            AlterColumn("dbo.Orders", "CouponId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Orders", "CouponId");
+            AddForeignKey("dbo.Orders", "CouponId", "dbo.Coupons", "Id", cascadeDelete: true);
+        }
+    }
+}
