200779e835fa1c5d9d0da9e274673fd09c246624
Anaraki-mh <m.anaraki.1382@gmail.com>
2023-02-17 12:20:50 +0330

    Added a new keyvalue pair for the layout

--- Dokan.Web/Helpers/LayoutHelper.cs
+++ Dokan.Web/Helpers/LayoutHelper.cs
@@ -16,6 +16,7 @@ namespace Dokan.Web.Helpers
 
         private static IKeyValueContentService _kvContentService { get; set; }
         private static IMenuService _menuService { get; set; }
+        private static IProductCategoryService _productCategoryService { get; set; }
 
         private static bool _initialized = false;
 
@@ -29,8 +30,10 @@ namespace Dokan.Web.Helpers
         public static string FooterWidgetTwoTitle { get; set; }
         public static string FooterWidgetTwoList { get; set; }
 
-        public static List<Menu> MenuList { get; set; } 
-        public static List<Menu> MenuParentsList { get; set; } 
+        public static List<Menu> MenuList { get; set; }
+        public static List<Menu> MenuParentsList { get; set; }
+
+        public static List<ProductCategory> ProductCategoryParentsList { get; set; }
 
         #endregion
 
@@ -41,6 +44,7 @@ namespace Dokan.Web.Helpers
         {
             _kvContentService = DependencyResolver.Current.GetService<IKeyValueContentService>();
             _menuService = DependencyResolver.Current.GetService<IMenuService>();
+            _productCategoryService = DependencyResolver.Current.GetService<IProductCategoryService>();
         }
 
         #endregion
@@ -50,7 +54,7 @@ namespace Dokan.Web.Helpers
 
         public static void PrepareLayout()
         {
-            if(_initialized) 
+            if (_initialized)
                 return;
 
             Phone = Task.Run(() => _kvContentService.GetValueByKeyAsync("@phone")).Result;
@@ -66,7 +70,9 @@ namespace Dokan.Web.Helpers
             MenuList = Task.Run(() => _menuService.ListAsync()).Result;
             MenuParentsList = Task.Run(() => _menuService.ListAsync()).Result
                 .Where(x => x.ParentId == 0 || x.ParentId == null).OrderBy(x => x.Priority).ToList();
-            
+            ProductCategoryParentsList = Task.Run(() => _productCategoryService.ListAsync()).Result
+                .Where(x => x.ParentId == 0 || x.ParentId == null).OrderBy(x => x.Priority).ToList();
+
 
             _initialized = true;
         }
